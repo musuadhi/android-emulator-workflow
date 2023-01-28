d683128fc334ac7117bffcdb8db9f296521445ed
musuadhi <it.mrbharath@gmail.com>
2023-01-28 07:16:57 +0100

    add-emulator-branch

--- scripts/RunAppiumServer.sh
+++ scripts/RunAppiumServer.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -ex
 npm install -g appium@next
-appium driver install uiautomator2
 appium driver install xcuitest
 appium driver install espresso
-#appium driver install flutter
-
+appium driver install flutter
 
 appium -v
 appium --log appium.log &>/dev/null &
--- scripts/emulator-up.sh
+++ scripts/emulator-up.sh
@@ -4,20 +4,21 @@
 
 set -e #stop immediately if any error happens
 
-avd_name=$1
+sudo $ANDROID_HOME/tools/bin/sdkmanager --update
+$ANDROID_HOME/tools/bin/sdkmanager --install "emulator"
+$ANDROID_HOME/tools/bin/sdkmanager --install "system-images;android-29;google_apis;x86"
+echo "no" | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "pixel" --device "pixel" --package "system-images;android-29;google_apis;x86" --tag "google_apis" --abi "x86"
+$ANDROID_HOME/emulator/emulator-headless @pixel -no-boot-anim -netdelay none -no-snapshot -wipe-data -verbose -show-kernel -no-audio -gpu swiftshader_indirect -no-snapshot &> /tmp/log.txt &
+sleep 160
 
-if [[ -z "$avd_name" ]]; then
-  avd_name="avd28"
-fi
+$ANDROID_HOME/platform-tools/adb wait-for-device
 
-#check if emulator work well
-emulator -version
+$ANDROID_HOME/platform-tools/adb devices
 
-# create virtual device, default using Android 9 Pie image (API Level 28)
-echo no | avdmanager create avd -n avd28 -k "system-images;android-28;google_apis;x86_64"
 
-# start the emulator
-emulator -avd avd28 -no-audio -no-window -no-accel &
-
-# show connected virtual device
-adb devices
+sudo npm install -g appium@next
+appium driver install xcuitest
+appium driver install espresso
+appium driver install flutter
+appium -v
+appium --log appium.log &>/dev/null &
